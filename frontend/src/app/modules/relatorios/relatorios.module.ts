dc35d22846a792843a8b2334dfde41a57cc69f53
Danilo Rodrigues Teixeira <danilo.rt@outlook.com>
2021-09-15 14:26:47 -0300

    Correção bug header

--- frontend/src/app/modules/relatorios/relatorios.module.ts
+++ frontend/src/app/modules/relatorios/relatorios.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { RelatoriosRoutingModule } from './relatorios-routing.module';
+
 import { RelatoriosComponent } from './relatorios.component';
 import { RelatoriosService } from './services/relatorios';
 
